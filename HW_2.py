83a388376e0a8ef412c532b30a3352f6a93db407
Kate Nesterovich <Nesterka77@tut.by>
2022-02-02 20:38:56 +0300

    add HW_2.py

---
+++ HW_2.py
@@ -0,0 +1,17 @@
+# 1. Создать переменную item_1 типа integer.
+item_1 = int(5)
+# 2. Создать переменную item_2 типа integer.
+item_2 = int(10)
+# 3. Создать переменную result_sum в которой вы суммируете item_1 и item_2.
+result_sum = item_1 + item_2
+# 4. Вывести result_sum в консоль
+print(result_sum)
+# 5.Создать переменную result_subtr в которой вы вычитаете большую по
+# значению переменную из меньшей по значению.
+result_subtr = item_2 - item_1
+# 6. Вывести result_subtr в консоль.
+print(result_subtr)
+# 7. Создать переменную result_multi в которой вы умножаете item_1 на item_2.
+result_multi = item_1 * item_2
+# 6. Вывести result_multi в консоль.
+print(result_multi)
